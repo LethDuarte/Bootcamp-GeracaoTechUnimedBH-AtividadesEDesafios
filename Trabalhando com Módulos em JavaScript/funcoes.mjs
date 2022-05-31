7acd83732731a7a32a5b7a8b37af9992537926f4
Letícia Duarte Rosa <lethduarte@gmail.com>
2022-05-31 17:25:07 -0300

    modulos e typescript

---
+++ Trabalhando com Módulos em JavaScript/funcoes.mjs
@@ -0,0 +1,14 @@
+function mostraIdade (nome, idade) {
+    return `A idade de ${nome} é ${idade} anos.`
+}
+
+function mostraCidade (nome, cidade) {
+    return `A idade de ${nome} é ${cidade}.`
+}
+
+function mostraHobby (nome, hobby) {
+    return `O idade de ${nome} é ${hobby}.`
+}
+
+export { mostraIdade, mostraCidade, mostraHobby };
+
---
+++ Trabalhando com Módulos em JavaScript/main.mjs
@@ -0,0 +1,6 @@
+import {  mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Letícia", 28))
+console.log(mostraCidade("Letícia", "São Gonçalo"))
+console.log(mostraHobby("Letícia", "ler livros"))
+
