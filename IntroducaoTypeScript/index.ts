04205b20137135e0a0fa82b70a4483779fc8011e
LethDuarte <lethduarte@gmail.com>
2022-06-12 15:22:42 -0300

    salvando1

--- IntroducaoTypeScript/index.ts
+++
@@ -1,7 +0,0 @@
-const input = document.getElementById('input') as HTMLInputElement;
-
-input.addEventListener('input', (event) => {
-    //console.log('Digitei')
-    const i = event.currentTarget as HTMLInputElement;
-    console.log(i.value)
-})
--- Trabalhando com Módulos em JavaScript/main.mjs
+++
@@ -1,6 +0,0 @@
-import {  mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
-
-console.log(mostraIdade("Letícia", 28))
-console.log(mostraCidade("Letícia", "São Gonçalo"))
-console.log(mostraHobby("Letícia", "ler livros"))
-
