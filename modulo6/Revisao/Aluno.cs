4d83665388588b72147d2c7152e95ef50f950537
LethDuarte <lethduarte@gmail.com>
2022-07-13 17:16:14 -0300

    revisao

---
+++ modulo6/Revisao/Aluno.cs
@@ -0,0 +1,8 @@
+namespace Revisao
+{
+    public struct Aluno // o ideal é que seja uma class, mas esse exemplo é muito simples
+    {
+        public string Nome { get; set; }
+        public decimal Nota { get; set; }
+    }
+}
