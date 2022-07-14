8d6b0b1208ec79cc85321d3c5e014fb8200a1a02
LethDuarte <lethduarte@gmail.com>
2022-07-14 14:50:05 -0300

    aula 3 .NET e LINQ

--- modulo6/LINQ E NET/Colecoes/Program.cs
+++ modulo6/LINQ E NET/Colecoes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Colecoes.Helper;
 
 namespace Colecoes
 {
@@ -6,10 +7,77 @@ namespace Colecoes
     {
         static void Main(string[] args)
         {
+            OperacoesArray op = new OperacoesArray();
+            int[] array = new int[5] {6, 3, 8, 1, 9};
+            int [] arrayCopia = new int[10];
+            string[] arrayString = op.ConverterParaArrayString(array);
+
+            //int valorProcurado = 9;
+
+            System.Console.WriteLine($"Capacidade atual do array: {array.Length}");
+
+            op.RedimensionarArray(ref array, array.Length*2);
+
+            System.Console.WriteLine($"Capacidade atual do array após redimensionar: {array.Length}");
+
+            // int indice = op.ObterIndice(array, valorProcurado);
+
+            // if(indice > -1){
+            //     System.Console.WriteLine("O indice do elemento {0} é: {1}", valorProcurado, indice);
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine("Valor não existente no array.");
+            // }
+
+            // int valorAchado = op.ObterValor(array, valorProcurado);
+
+            // if (valorAchado > 0) //qd o valor não é encontrado, .find retorna o valor padrão do array (no caso 0)
+            // {
+            //     System.Console.WriteLine("Encontrei o valor");
+            // }
+            // else{
+            //     System.Console.WriteLine("Não encontrei o valor");
+            // }
+
+            // bool todosMaiorQue = op.TodosMaiorQue(array, valorProcurado);
+
+            // if(todosMaiorQue){
+            //     System.Console.WriteLine("Todos os valores são maiores que {0}", valorProcurado);
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine("Existem valores que não são maiores que {0}", valorProcurado);
+            // }
+            //{0} é substituido pela variavel passada em seguida
+
+
+            // bool existe = op.Existe(array, valorProcurado);
+
+            // if(existe)
+            // {
+            //     System.Console.WriteLine("Encontrei o valor {0}", valorProcurado);
+            // } 
+            // else
+            // {
+            //     System.Console.WriteLine("Não encontei o valor {0}", valorProcurado);
+            // }
+
+            // System.Console.WriteLine("Array original:");
+            // op.ImprimirArray(array);
             
+            //op.OrdenarBubbleSort(ref array);
+            //op.Ordernar(ref array);
 
+            // System.Console.WriteLine("Array ordenado:");
+            // op.ImprimirArray(array);
 
+            // System.Console.WriteLine("Array antes da copia");
+            // op.ImprimirArray(arrayCopia);
 
+            // op.Copiar(ref array, ref arrayCopia);
+            // System.Console.WriteLine("Array depois da copia:");
+            // op.ImprimirArray(arrayCopia);
 
 
             //declarando um array multidimensional com 4 lns e 2 cols
