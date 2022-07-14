0f24787100b627e5f4e41fbadb651c26bc9d87e2
LethDuarte <lethduarte@gmail.com>
2022-07-14 11:51:55 -0300

    aula 1 .NET e LINQ

---
+++ modulo6/LINQ E NET/Colecoes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+
+            //declarando um array multidimensional com 4 lns e 2 cols
+            // int[,] matriz = new int[4,2]
+            // {
+            //     {8, 8}, //linha 0 - colunas 0 e 1
+            //     {10, 20},  //linha 1 - colunas 0 e 1
+            //     {50, 100},  //linha 2 - colunas 0 e 1
+            //     {90, 200}   //linha 3 - colunas 0 e 1
+            // };
+
+            // for (int i = 0; i < matriz.GetLength(0); i++) //GetLength pega a dimensão do array - vai percorrer as linhas
+            // {
+            //     for (int j = 0; j < matriz.GetLength(1); j++) //vai percorrer as colunas
+            //     {
+            //         Console.WriteLine(matriz[i, j]);
+            //     }
+            // }
+
+            // int[] arrayInteiros = new int[3]; //declarei um array com capacidade máxima de 3posicoes
+
+            // //inserindo valor em cada uma das posições
+            // arrayInteiros[0] = 10;
+            // arrayInteiros[1] = 20;
+            // arrayInteiros[2] = 30;
+
+            // System.Console.WriteLine("Percorrendo o array pelo for:");
+            // for (int i = 0; i < arrayInteiros.Length; i++) //boa pratica usar sempre o .Length, que pega a capacidade total do array
+            // {
+            //     Console.WriteLine(arrayInteiros[i]);
+            // }
+
+            // System.Console.WriteLine("Percorrendo o array pelo foreach:");
+            // foreach (int item in arrayInteiros)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
+        }
+    }
+}
+
