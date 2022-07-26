fe977d907e7352c270d42be07a8e98cbe1a88343
LethDuarte <lethduarte@gmail.com>
2022-07-26 13:23:17 -0300

    materia6 atualizando

---
+++ modulo6/materia6/DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+var path = Path.Combine(Environment.CurrentDirectory, "globo");
+//criando diretorio
+var dirGlobo = Directory.CreateDirectory(path);
+//criando subdiretórios
+var dirAmNorte = dirGlobo.CreateSubdirectory("América do Norte");
+var dirAmCentral = dirGlobo.CreateSubdirectory("América Central");
+var dirAmSul = dirGlobo.CreateSubdirectory("América do Sul");
+
+dirAmNorte.CreateSubdirectory("USA");
+dirAmNorte.CreateSubdirectory("México");
+dirAmNorte.CreateSubdirectory("Canada");
+
+dirAmCentral.CreateSubdirectory("Costa Rica");
+dirAmCentral.CreateSubdirectory("Panamá");
+dirAmCentral.CreateSubdirectory("El Salvador");
+
+dirAmSul.CreateSubdirectory("Brasil");
+dirAmSul.CreateSubdirectory("Argentina");
+dirAmSul.CreateSubdirectory("Paraguai");
--- modulo6/materia6/FileAndFileInfo/Program.cs
+++ modulo6/materia6/FileAndFileInfo/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using System.IO;
+
+WriteLine("Digite o nome do arquivo: ");
+var nome = ReadLine();
+nome = LimparNome(nome);
+var path = Path.Combine(Environment.CurrentDirectory, $"{nome}.txt");
+
+CriarArquivo(path);
+WriteLine("Aperte ENTER para finalizar.");
+ReadLine();
+
+static string LimparNome(string nome)
+{
+    foreach(var @char in Path.GetInvalidFileNameChars())
+        {
+            nome = nome.Replace(@char, '-');
+        }
+        return nome;
+}
+
+static void CriarArquivo(string path)
+{
+    try
+    {
+        using var sw = File.CreateText(path);
+        sw.WriteLine("Esta é a linha 1 do arquivo");
+        sw.WriteLine("Esta é a linha 1 do arquivo");
+        sw.WriteLine("Esta é a linha 1 do arquivo");
+    }
+    catch 
+    {
+        WriteLine("O nome do arquivo é inválido.");
+    }
+    
+}
+
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// var path = Path.Combine(Environment.CurrentDirectory, "teste.txt");
+
+// //File.Create(path);
+
+// //cria um texto dentro do arquivo
+// using (var sw = File.CreateText(path))
+// {
+//     sw.WriteLine("Esta é a linha 1 do arquivo.");
+//     sw.WriteLine("Esta é a linha 2 do arquivo.");
+//     sw.WriteLine("Esta é a linha 3 do arquivo.");
+// }
+
+//sw.Flush();
+
